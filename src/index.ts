edf2dd1b44d57aa5d0be3542cf596ecf538a57e9
dario zubaray <dzubaray@gmail.com>
2020-03-30 13:45:31 -0300

    Nuestro primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-
+import { Observable } from 'rxjs';
 
 console.log('Hola Mundo!');
 
+// const obs$ = Observable.create();
 
+const obs$ = new Observable( subs => {
 
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    subs.complete();
 
+});
 
-
+obs$.subscribe( console.log );
