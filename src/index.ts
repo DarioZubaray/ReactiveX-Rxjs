a9dd7dfe5278320242deec4bc002ce12de230d6c
dario zubaray <dzubaray@gmail.com>
2020-04-03 14:31:56 -0300

    Observer y subscriber, video 14

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
-console.log('Hola Mundo!');
+console.log('Hola ReactiveX!');
 
 // const obs$ = Observable.create();
 
-const obs$ = new Observable( subs => {
+const obs$ = new Observable<string>( subs => {
 
     subs.next('Hola');
     subs.next('Mundo');
 
     subs.complete();
 
+    // No se ejecutan
+    subs.next('Hola');
+    subs.next('Mundo');
 });
 
-obs$.subscribe( console.log );
+// obs$.subscribe( console.log );
+
+// obs$.subscribe( 
+//     valor => console.log('next: ', valor),
+//     error => console.error(error),
+//     () => console.info('completado')
+//  );
+
+const observador: Observer<any> = {
+    next: value => console.log('siguiente: ', value),
+    error: error => console.error('error: ', error),
+    complete: () => console.info('completado')
+};
+
+obs$.subscribe( observador );
